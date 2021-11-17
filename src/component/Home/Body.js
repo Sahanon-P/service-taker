5850b6f4f9268be91e997b452f684626f8202737
Sahanon Phisetpakasit <sahanon.p@ku.th>
2021-11-17 21:26:40 +0700

    add Home

---
+++ src/component/Home/Body.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Body.css'
+import {Link} from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
+
+
+const Body = () => {
+    return (
+        <div className="container">
+            <table>
+                <tr>
+                    <th colspan="2">
+                        <div className="box">
+                            <h2><Link to="/detail">My vaccine</Link></h2>
+                        </div>
+                    </th>
+                </tr>
+                <tr>
+                    <td><div className="box">
+                        <h2>Walk In</h2>
+                    </div></td>
+                    <td><div className="box">
+                        <h2><Link to="/contact">Contact Us</Link></h2>
+                    </div></td>
+                </tr>
+            </table>
+        </div>
+    )
+};
+
+export default Body;
